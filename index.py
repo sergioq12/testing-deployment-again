eb691f517b40108120b66b4cbcf11418942288c5
sergioq12 <sergioxq12@gmail.com>
2023-06-28 19:55:47 -0400

    first commmit

---
+++ index.py
@@ -0,0 +1,34 @@
+# Getting started lets gooo
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from bcrypt import gensalt, hashpw, checkpw
+import jwt
+import os
+from dotenv import load_dotenv
+import sys
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+# Loads 
+load_dotenv()
+
+# Creates FastAPI app
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000/",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def test_endpoint():
+    return {"message": "This is the main test"}
